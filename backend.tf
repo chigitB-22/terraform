71481b9c1c4990c7b641d17efa44c32b6d2b67d7
chigitB-22 <105946372+chigitB-22@users.noreply.github.com>
2022-08-04 19:01:50 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
  terraform {
    backend "s3" {
     bucket = "code-sharing-bucket"
-    key    = "avinash/s3file-3.tfstate"
+    key    = "chidi/s3file-3.tfstate"
     region = "us-east-2"
     profile = "jespo"
 dynamodb_table= "firstprojecttable"
